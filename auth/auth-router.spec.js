ab176bad9010c498266a309280137faa57ba9915
MelvinPeepers <peepersm@gmail.com>
2019-09-14 15:14:07 -0400

    second test done

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -20,4 +20,16 @@ describe("POST /register", () => {
         expect(res.type).toMatch(/json/);
       });
   });
+
+  it("should return 200", () => {
+    return request(server)
+      .post("/api/auth/login")
+      .send({
+        username: "Liza",
+        password: "abc12d3"
+      })
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
 });
